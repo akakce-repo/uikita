443a527b7896d151a2cee22d9ecd399e5b6f2a1a
enes <enes@akakce.com>
2022-04-04 09:45:23 +0300

    build.gradle versiyonu yükseltildi.

--- uikit/src/main/java/com/akakce/uikit/AkakceUIText.kt
+++ uikit/src/main/java/com/akakce/uikit/AkakceUIText.kt
@@ -1,12 +1,10 @@
 package com.akakce.uikit
 
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.TextUnit
