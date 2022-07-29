f17c1b3363e4c30cf8aa834fbaef44831f5f1777
selimsimsek <selim@akakce.com>
2022-07-29 17:44:58 +0300

    Card componnet yapısından kaldırıldı.

--- uikit/src/main/java/com/akakce/uikit/component/Component.kt
+++ uikit/src/main/java/com/akakce/uikit/component/Component.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 /**
@@ -27,35 +28,33 @@ class AkakceUIComponent {
         badgeText: String,
         onClick: Boolean
     ) {
-        Box {
-            Card(
-                shape = RoundedCornerShape(8.dp),
-                modifier = Modifier
-                    .size(205.dp, 290.dp)
-                    .padding(all = 8.dp)
-                    .fillMaxWidth()
-            ) {
-                Box {
-                    Column {
-                        AkakceUIProductImage().ProductImage(imageUrl)
-                        AkakceUIProductName().ProductName(productName)
-                        AkakceUIDiscountCell().DiscountCell(cheaperText, percent, isDown)
-                        AkakceUIPriceCell().PriceCell(price, countPrice)
-                        AkakceUIUnitPrice().UnitPrice(unitPrice)
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        AkakceUIBadge().Badge(badgeText)
-                        AkakceUIBell().Bell(onClick)
-
-                    }
+        Box(
+            modifier = Modifier
+                .size(205.dp, 290.dp)
+                .padding(all = 8.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+        ) {
+            Box {
+                Column {
+                    AkakceUIProductImage().ProductImage(imageUrl)
+                    AkakceUIProductName().ProductName(productName)
+                    AkakceUIDiscountCell().DiscountCell(cheaperText, percent, isDown)
+                    AkakceUIPriceCell().PriceCell(price, countPrice)
+                    AkakceUIUnitPrice().UnitPrice(unitPrice)
                 }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    AkakceUIBadge().Badge(badgeText)
+                    AkakceUIBell().Bell(onClick)
 
+                }
             }
 
         }
+
     }
 }
