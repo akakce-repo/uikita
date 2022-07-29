07a4b9fef97f96d3972b66638d519c2afacb07f1
selimsimsek <selim@akakce.com>
2022-07-29 15:23:12 +0300

    Component fonksiyonları birleştirildi.

--- uikit/src/main/java/com/akakce/uikit/component/Component.kt
+++ uikit/src/main/java/com/akakce/uikit/component/Component.kt
@@ -1,15 +1,24 @@
 package com.akakce.uikit.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.integerResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.*
+import coil.compose.rememberImagePainter
+import com.akakce.uikit.R
+import com.akakce.uikit.ui.theme.AkakceTheme
 
 /**
  * @Author: Selim Simsek
@@ -18,7 +27,7 @@ import androidx.compose.ui.unit.dp
 
 
 class AkakceUIComponent {
-    @OptIn(ExperimentalMaterialApi::class)
+    @OptIn(ExperimentalMaterialApi::class, ExperimentalUnitApi::class)
     @Composable
     fun Component(
         imageUrl: String,
@@ -41,19 +50,123 @@ class AkakceUIComponent {
         ) {
             Box {
                 Column {
-                    AkakceUIProductImage().ProductImage(imageUrl)
-                    AkakceUIProductName().ProductName(productName)
-                    AkakceUIDiscountCell().DiscountCell(cheaperText, percent, isDown)
-                    AkakceUIPriceCell().PriceCell(price, countPrice)
-                    AkakceUIUnitPrice().UnitPrice(unitPrice)
+                    Image(
+                        painter = rememberImagePainter(imageUrl), contentDescription = null,
+                        contentScale = ContentScale.Inside,
+                        modifier = Modifier
+                            .padding(9.dp, 15.dp, 9.dp, 0.dp)
+                            .size(180.dp, 165.dp)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        text = productName,
+                        modifier = Modifier.padding(9.dp, 0.dp),
+                        color = AkakceTheme.colors.akakceBlueGray500,
+                        fontSize = 13.sp,
+                        maxLines = 2
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = cheaperText,
+                            style = MaterialTheme.typography.subtitle2,
+                            fontSize = 9.sp,
+                            modifier = Modifier.padding(9.dp, 1.dp),
+                            fontWeight = FontWeight.Bold,
+                            color = AkakceTheme.colors.akakceBlueGray600
+                        )
+                        if (isDown) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_downarrow),
+                                contentDescription = null,
+                                modifier = Modifier.padding(0.dp, 4.dp)
+                            )
+                        }
+
+                        Text(
+                            text = percent,
+                            style = MaterialTheme.typography.subtitle2,
+                            fontSize = 10.sp,
+                            color = AkakceTheme.colors.akakceBaseGreen,
+                            modifier = Modifier.padding(1.dp, 0.dp)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = price,
+                            style = MaterialTheme.typography.subtitle2,
+                            modifier = Modifier.padding(9.dp, 0.dp, 7.dp, 0.dp),
+                            color = AkakceTheme.colors.akakceBlueGray600
+                        )
+                        Text(
+                            text = countPrice,
+                            style = MaterialTheme.typography.subtitle1,
+                            fontSize = 10.sp,
+                            color = AkakceTheme.colors.akakceBaseBlue,
+                            modifier = Modifier.padding(12.dp, 4.dp),
+                            letterSpacing = TextUnit(0.6f, TextUnitType.Sp)
+                        )
+                    }
+                    Text(
+                        text = unitPrice,
+                        modifier = Modifier.padding(9.dp, 0.dp),
+                        color = AkakceTheme.colors.akakceBasePurple,
+                        fontSize = 10.sp
+                    )
                 }
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    AkakceUIBadge().Badge(badgeText)
-                    AkakceUIBell().Bell(onClick)
+                    Box {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_badge),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(104.dp, 38.dp)
+                                .padding(0.dp, 9.dp),
+                        )
+                        Text(
+                            text = badgeText,
+                            modifier = Modifier.padding(6.dp, 12.dp),
+                            color = AkakceTheme.colors.akakceBasePurple,
+                            fontSize = 9.sp,
+                            fontWeight = FontWeight.Bold,
+                            letterSpacing = TextUnit(0.3f, TextUnitType.Sp)
+                        )
+
+                    }
+                    Box {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_ellipse),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(38.dp)
+                                .padding(5.dp),  //avoid the oval shape ,
+                        )
+                        Image(
+                            painter = painterResource(
+                                id = if (onClick) {
+                                    R.drawable.ic_follow
+                                }else{
+                                    R.drawable.ic_unfollow
+                                }
+                            ),
+                            contentDescription = "content description",
+                            modifier = Modifier
+                                .padding(12.dp, 12.dp)
+                                .size(15.dp)
+                        )
+
+
+                    }
 
                 }
             }
