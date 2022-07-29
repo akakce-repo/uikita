d299597888ff6e6cf2b2c0dfc3ad2606a3f8e33a
selimsimsek <selim@akakce.com>
2022-07-30 01:35:34 +0300

    Takibe alma çanı düzenlendi.

--- uikit/src/main/java/com/akakce/uikit/component/Bell.kt
+++ uikit/src/main/java/com/akakce/uikit/component/Bell.kt
@@ -1,10 +1,11 @@
 package com.akakce.uikit.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -17,34 +18,40 @@ import com.akakce.uikit.R
  */
 
 class AkakceUIBell {
-    var clickable : Boolean = false
+    var clickable: Boolean = false
+
     @Composable
     fun Bell(onClick: Boolean) {
         Box {
-            Image(
-                painter = painterResource(id = R.drawable.ic_ellipse),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(38.dp)
-                    .padding(5.dp),  //avoid the oval shape ,
-            )
-            Image(
-                painter = painterResource(
-                    id = if (clickable) {
-                        R.drawable.ic_follow
-                    }else{
-                        R.drawable.ic_unfollow
-                    }
-                ),
-                contentDescription = "content description",
-                modifier = Modifier
-                    .padding(12.dp, 12.dp)
-                    .size(15.dp)
-                    .clickable {
-                        clickable = onClick
-                    }
-            )
+            Box(modifier = Modifier.padding(0.dp, 2.dp)) {
+                OutlinedButton(
+                    shape = RoundedCornerShape(
+                        20.dp,
+                        20.dp,
+                        20.dp,
+                        20.dp
+                    ),
+                    modifier = Modifier
+                        .size(41.dp)
+                        .padding(4.dp),
+                    onClick = { clickable = onClick }
+                ) {}
+                Image(
+                    painter = painterResource(
+                        id = if (clickable) {
+                            R.drawable.ic_follow
+                        } else {
+                            R.drawable.ic_unfollow
+                        }
+                    ),
+                    contentDescription = "content description",
+                    modifier = Modifier
+                        .padding(11.dp, 11.dp)
+                        .size(20.dp)
+                )
 
+
+            }
 
         }
     }
